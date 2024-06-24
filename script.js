88c8f18db645420eb59013590eef919ad214c501
Blex Olonde <86054041+blexolonde@users.noreply.github.com>
2024-06-24 12:52:42 +0300

    Organize

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('alertButton');
+  
+    button.addEventListener('click', function() {
+      alert('Button was clicked!');
+    });
+  });
+  
